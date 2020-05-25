723166446287628595a51b3c69955dfab21397da
Joachim Saul <saul@gfz-potsdam.de>
2020-05-25 11:02:20 +0200

    [notifier-logger] made it work again under Python 3...

--- playback/notifier/notifier-logger.py
+++ playback/notifier/notifier-logger.py
@@ -1,5 +1,7 @@
-import sys, os, gc, md5, logging, logging.handlers, StringIO
+#!/usr/bin/env seiscomp-python
+import sys, os, gc, hashlib, logging, logging.handlers
 import seiscomp.client, seiscomp.datamodel, seiscomp.io, seiscomp.logging
+from io import BytesIO
 
 
 def objectToXML(obj, expName = "trunk"):
@@ -25,17 +27,32 @@ def objectToXML(obj, expName = "trunk"):
     if not exp:
         seiscomp.logging.error("exporter '%s' not found" % expName)
         return None
+
     exp.setFormattedOutput(True)
 
     try:
-        io = StringIO.StringIO()
+        io = BytesIO()
         sink = Sink(io)
         exp.write(sink, obj)
         return io.getvalue().strip()
-    except Exception, e:
-        seiscomp.logging.error(error + str(e))
+    except Exception as err:
+        seiscomp.logging.error(str(err))
 
     return None
+
+
+def objectToXML_workaround(obj, formatted=True):
+    # This is a workaround that requires writing to a temporary file
+    ar = seiscomp.io.XMLArchive()
+    ar.setFormattedOutput(formatted)
+    # before we write to /tmp we try to write to ramdisk /dev/shm
+    for tempdir in ["/dev/shm", "/tmp"]:
+        tempfile = os.path.join(tempdir, "notifier-logger.xml")
+        if ar.create(tempfile):
+            ar.writeObject(obj)
+            ar.close()
+            xml = open(tempfile).read().strip()
+            return xml
 
 
 class MyLogHandler(logging.handlers.TimedRotatingFileHandler):
@@ -67,22 +84,36 @@ class NotifierLogger(seiscomp.client.Application):
         seiscomp.datamodel.PublicObject.SetRegistrationEnabled(False) 
         self._logger = logging.getLogger("Rotating Log")
         self._logger.setLevel(logging.INFO)
-        handler = MyLogHandler("notifier-log", when="d", interval=1, backupCount=48)
+
+    def createCommandLineDescription(self):
+        self.commandline().addGroup("Output")
+        self.commandline().addStringOption("Output", "prefix", "path/file prefix to generate output file names")
+        return True
+
+    def validateParameters(self):
+        if not seiscomp.client.Application.validateParameters(self):
+            return False
+        try:
+            self._prefix = self.commandline().optionString("prefix")
+        except:
+            self._prefix = "notifier-log"
+        handler = MyLogHandler(self._prefix, when="h", interval=1, backupCount=48)
         self._logger.addHandler(handler)
+        return True
 
     def _writeNotifier(self, xml):
         now = seiscomp.core.Time.GMT().toString("%Y-%m-%dT%H:%M:%S.%f000000")[:26]+"Z"
-        self._logger.info("####  %s  %s  %d bytes" % (now, md5.new(xml).hexdigest(), len(xml)))
+        h = hashlib.md5(xml.encode()).hexdigest()
+        self._logger.info("####  %s  %s  %d bytes" % (now, h, len(xml)))
         self._logger.info(xml)
         gc.collect()
 
     def handleMessage(self, msg):
         nmsg = seiscomp.datamodel.NotifierMessage.Cast(msg)
         if nmsg:
-            xml = objectToXML(nmsg)
+            xml = objectToXML_workaround(nmsg)
             if xml:
                 self._writeNotifier(xml)
-#       seiscomp.client.Application.handleMessage(self, msg)
 
 
 if __name__ == "__main__":
--- playback/notifier/run-notifier-logger.sh
+++ playback/notifier/run-notifier-logger.sh
@@ -4,4 +4,4 @@ d=$HOME/log/notifiers
 p=$d/notifier-log
 mkdir -p $d
 
-$HOME/seiscomp/bin/seiscomp exec ./notifier-logger.py -H geofon-proc.gfz-potsdam.de -u ntflog2 --debug --prefix=$p
+$HOME/seiscomp/bin/seiscomp exec seiscomp-python notifier-logger.py -H geofon-proc.gfz-potsdam.de -u ntflog2 --debug --prefix=$p
