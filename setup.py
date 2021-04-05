e91ce4b878f48e32fd0b1ad88df3b5c91cd35061
Joachim Saul <saul@gfz-potsdam.de>
2021-04-05 17:29:08 +0200

    added a few utility functions

--- setup.py
+++ setup.py
@@ -14,6 +14,7 @@ setup(
         'scstuff' : [
             "__init__.py",
             "util.py",
+            "dbutil.py",
             "mtutil.py",
             "inventory",
             "eventloader.py"
