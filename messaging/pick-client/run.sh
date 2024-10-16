01446c55566b41f463339a3af30faa3390abbcdf
saul <saul@gfz-potsdam.de>
2024-10-16 19:34:52 +0200

    Moved pick-client and pick-sender to scexamples

--- messaging/pick-client/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# The host to connect to and receive the messages from
-host="geofon-proc"
-
-exec ~/seiscomp/bin/seiscomp exec seiscomp-python \
-    pick-client.py -u xyzabc -H "$host" --debug
